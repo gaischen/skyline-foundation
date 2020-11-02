d450aaf73ca39acbd99c1ca3661ff504a0e27666
vanga <gaischen@163.com>
2020-11-02 08:31:43 +0800

    buf

--- network/transport/bytebuf.go
+++ network/transport/bytebuf.go
@@ -3,3 +3,18 @@ package transport
 type ByteBuf struct {
 	data []byte
 }
+
+func (b *ByteBuf) Bytes() []byte {
+	return b.data
+}
+
+func (b *ByteBuf) Len() int {
+	return len(b.data)
+}
+
+func (b *ByteBuf) Resize(n int) {
+	if n > cap(b.data) {
+		b.Resize(n)
+	}
+	b.data = b.data[0:n]
+}
