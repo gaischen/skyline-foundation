a1015b41521e4aaff1b2479c8c6d0ea9d5b5df0e
vanga <gaischen@163.com>
2020-11-13 09:52:44 +0800

    http2Server

---
+++ webserver/credentials/credentials.go
@@ -0,0 +1,5 @@
+package credentials
+
+type AuthInfo interface {
+
+}
--- webserver/http2/http2_server.go
+++ webserver/http2/http2_server.go
@@ -1,3 +1,20 @@
 package http2
 
+import (
+	"context"
+	"github.com/vanga-top/skyline-foundation/webserver/credentials"
+	"net"
+)
 
+type http2Server struct {
+	id           string
+	ctx          context.Context
+	conn         net.Conn
+	remoteAddr   net.Addr
+	localAddr    net.Addr
+	maxStreamID  uint32
+	authInfo     credentials.AuthInfo
+	writableChan chan int //sync write
+	shutdownChan chan struct{}
+
+}
