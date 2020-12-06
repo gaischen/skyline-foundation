237336a00cbe3253858fee916e18092e7a0c5a3b
gaischen <gaischen@163.com>
2020-12-06 08:21:40 +0800

    Update buntdb.go
    db status

--- storage/buntdb/buntdb.go
+++ storage/buntdb/buntdb.go
@@ -12,5 +12,17 @@ type Config struct {
 }
 
 type DB struct {
-
+	mu        sync.RWMutex      // the gatekeeper for all fields
+	file      *os.File          // the underlying file
+	buf       []byte            // a buffer to write to
+	keys      *btree.BTree      // a tree of all item ordered by key
+	exps      *btree.BTree      // a tree of items ordered by expiration
+	idxs      map[string]*index // the index trees.
+	exmgr     bool              // indicates that expires manager is running.
+	flushes   int               // a count of the number of disk flushes
+	closed    bool              // set when the database has been closed
+	config    Config            // the database configuration
+	persist   bool              // do we write to disk
+	shrinking bool              // when an aof shrink is in-process.
+	lastaofsz int               // the size of the last shrink aof size
 }
