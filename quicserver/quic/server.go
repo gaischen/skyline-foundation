3ac92b8c2c746a53e510e5fb3f078ddac524470c
gaischen <gaischen@163.com>
2020-12-20 07:45:37 +0800

    basicServer

--- quicserver/quic/server.go
+++ quicserver/quic/server.go
@@ -1,9 +1,25 @@
 package quic
 
-import "crypto/tls"
+import (
+	"crypto/tls"
+	"net"
+	"sync"
+)
 
 //start listen quic addr
 func ListenAddr(addr string, tlsConfig *tls.Config, config *Config) (Listener, error) {
 
 	return nil, nil
+}
+
+type basicServer struct {
+	mutex               sync.Mutex
+	acceptEarlySessions bool
+	tlsConfig           *tls.Config
+	config              *Config
+
+	conn net.PacketConn
+	//if the server is started with listenAddr we create a packet conn.
+	//if it is started with listen we take a packet conn as a parameter
+	createdPacketConn bool
 }
