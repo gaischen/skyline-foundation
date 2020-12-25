298d79363db1b130a4e3b0474637babaee4b9b90
gaischen <gaischen@163.com>
2020-12-25 10:37:38 +0800

    setReceiveBuffer

--- quicserver/quic/packet_handler_map.go
+++ quicserver/quic/packet_handler_map.go
@@ -1,6 +1,7 @@
 package quic
 
 import (
+	"errors"
 	"github.com/vanga-top/skyline-foundation/quicserver/quic/logging"
 	"github.com/vanga-top/skyline-foundation/quicserver/quic/utils"
 	"net"
@@ -11,5 +12,16 @@ func newPacketHandlerMap(c net.PacketConn,
 	statelessResetKey []byte,
 	tracer logging.Tracer,
 	logger utils.Logger) (packetHandlerManager, error) {
-	//todo
+	if err := setReceiveBuffer(c, logger); err != nil {
+
+	}
+}
+
+func setReceiveBuffer(c net.PacketConn, logger utils.Logger) error {
+	conn, ok := c.(interface{ SetReadBuffer(int) error })
+	if !ok {
+		return errors.New("connection doesn't allow setting of receive buffer")
+	}
+
+
 }
